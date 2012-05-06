7f8777b9518a30f094dde7fbe6b1f70d4ea7bc68
Felix Pfeiffer <info@felixpfeiffer.com>
2012-05-06 19:28:51 +0200

    First Commit

---
+++ FormColEnd.php
@@ -0,0 +1,80 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * TYPOlight Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Felix Pfeiffer : Neue Medien 2007 - 2012
+ * @author     Felix Pfeiffer <info@felixpfeiffer.com>
+ * @package    Subcolumns
+ * @license    CC-A 2.0
+ * @filesource
+ */
+
+
+/**
+ * Class FormColPart
+ *
+ * Form field "explanation".
+ * @copyright  Felix Pfeiffer : Neue Medien 2010
+ * @author     Felix Pfeiffer <info@felixpfeiffer.com>
+ * @package    Subcolumns
+ */
+class FormColEnd extends Widget
+{
+
+	/**
+	 * Template
+	 * @var string
+	 */
+	protected $strTemplate = 'form_colset';
+	protected $strColTemplate = 'ce_colsetEnd';
+
+
+	/**
+	 * Do not validate
+	 */
+	public function validate()
+	{
+		return;
+	}
+
+
+	/**
+	 * Generate the widget and return it as string
+	 * @return string
+	 */
+	public function generate()
+	{
+		$this->strSet = $GLOBALS['TL_CONFIG']['subcolumns'] ? $GLOBALS['TL_CONFIG']['subcolumns'] : 'yaml3';
+		
+		if (TL_MODE == 'BE')
+		{
+			$objTemplate = new BackendTemplate('be_wildcard');
+			$objTemplate->wildcard = '### Subcolumns End <strong>'.$this->fsc_name.'</strong> ###';
+			
+			return $objTemplate->parse();
+		}
+		
+		$objTemplate = new FrontendTemplate($this->strColTemplate);
+		$objTemplate->useInside = $GLOBALS['TL_SUBCL'][$this->strSet]['inside'];
+		return $objTemplate->parse();
+	}
+}
+
+?>
